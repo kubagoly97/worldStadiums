a1feb9412e3cea2cd95ffad28ad96b66a6bc5768
Jakub Goły <kuba@MacBook-Air-Jakub.local>
2023-07-14 08:47:35 +0200

    add start script

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err, statusCode });
     console.log(err);
 })
-
-app.listen(3000, () => {
-    console.log('PORT 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 })
